9930f0fdd87468dc41d9dbc9cb70c8c52a3b4a59
yaroslav-kovalchyk <yaroslav.kovalchik@gmail.com>
2015-06-26 13:11:52 +0300

    'include' parameter for JDBC metadata builder

--- tamanoir-jdbc/src/main/java/com/jaspersoft/tamanoir/jdbc/JdbcMetadataBuilder.java
+++ tamanoir-jdbc/src/main/java/com/jaspersoft/tamanoir/jdbc/JdbcMetadataBuilder.java
@@ -45,7 +45,7 @@ import java.util.Set;
  * @author Yaroslav.Kovalchyk
  */
 public class JdbcMetadataBuilder implements MetadataBuilder<JdbcConnectionContainer> {
-    private static final Map<Integer, String> JDBC_TYPES_BY_CODE = Collections.unmodifiableMap(new HashMap<Integer, String>(){{
+    private static final Map<Integer, String> JDBC_TYPES_BY_CODE = Collections.unmodifiableMap(new HashMap<Integer, String>() {{
         put(Types.BIGINT, "java.lang.Long");
         put(Types.BIT, "java.lang.Boolean");
         put(Types.BOOLEAN, "java.lang.Boolean");
@@ -72,98 +72,122 @@ public class JdbcMetadataBuilder implements MetadataBuilder<JdbcConnectionContai
 
     @Override
     public MetadataItem build(JdbcConnectionContainer connection, Map<String, String[]> options) {
-        final MetadataGroupItem result = new MetadataGroupItem().setName("root");
         final String[] expands = options != null ? options.get("expand") : null;
-        final Map<String, List<String[]>> expandsMap = new HashMap<String, List<String[]>>();
-        if(expands != null){
-            for(String expand : expands){
-                final String[] tokens = expand.split("\\.");
-                List<String[]> tokensList = expandsMap.get(tokens[0]);
-                if(tokensList == null){
-                    tokensList = new ArrayList<String[]>();
-                    expandsMap.put(tokens[0], tokensList);
-                }
-                if(tokens.length > 1){
-                    tokensList.add(Arrays.copyOfRange(tokens, 1, tokens.length));
-                }
-            }
-        }
+        final String[] includes = options != null ? options.get("include") : null;
+        List<MetadataItem> items;
         try {
             final DatabaseMetaData metaData = connection.getConnection().getMetaData();
-            final ResultSet schemas = metaData.getSchemas();
-            while (schemas.next()) {
-                String schema = schemas.getString("TABLE_SCHEM");
-                final MetadataGroupItem schemaItem = new MetadataGroupItem().setName(schema);
-                result.addItem(schemaItem);
-                if(expandsMap.containsKey(schema)){
-                    schemaItem.setItems(expandSchema(schema, expandsMap.get(schema), metaData));
+            if (includes != null && includes.length > 0) {
+                items = includeMetadata(includes, metaData);
+            } else {
+                final Map<String, List<String[]>> expandsMap = new HashMap<String, List<String[]>>();
+                if (expands != null) {
+                    for (String expand : expands) {
+                        final String[] tokens = expand.split("\\.");
+                        List<String[]> tokensList = expandsMap.get(tokens[0]);
+                        if (tokensList == null) {
+                            tokensList = new ArrayList<String[]>();
+                            expandsMap.put(tokens[0], tokensList);
+                        }
+                        if (tokens.length > 1) {
+                            tokensList.add(Arrays.copyOfRange(tokens, 1, tokens.length));
+                        }
+                    }
                 }
+                items = expandMetadata(expandsMap, metaData);
             }
         } catch (SQLException e) {
             throw new ConnectionException(e);
         }
 
+        return items.size() == 1 ? items.get(0) : new MetadataGroupItem().setName("root").setItems(items);
+    }
+
+    protected List<MetadataItem> expandMetadata(Map<String, List<String[]>> expandsMap, DatabaseMetaData metaData) throws SQLException {
+        List<MetadataItem> result = new ArrayList<MetadataItem>();
+        final ResultSet schemas = metaData.getSchemas();
+        while (schemas.next()) {
+            String schema = schemas.getString("TABLE_SCHEM");
+            result.add(getSchemaMetadata(schema, expandsMap.get(schema), metaData));
+        }
+        return result;
+
+    }
+
+    protected List<MetadataItem> includeMetadata(String[] includes, DatabaseMetaData metaData) {
+        List<MetadataItem> result = new ArrayList<MetadataItem>();
+        for (String include : includes) {
+            if (include != null) {
+                final String[] path = include.split("\\.");
+                switch (path.length) {
+                    case 1:
+                        result.add(getSchemaMetadata(path[0], new ArrayList<String[]>(), metaData));
+                        break;
+                    case 2:
+                        result.add(getTableMetadata(path[0], path[1], true, metaData));
+                        break;
+                }
+            }
+        }
         return result;
     }
 
-    protected List<MetadataItem> expandSchema(String schema, List<String[]> tokensList, DatabaseMetaData metaData){
-        final List<MetadataItem> tableItems = new ArrayList<MetadataItem>();
-        final Set<String> tableNamesToExpand = new HashSet<String>();
-        if (tokensList != null){
-            for(String[] strings : tokensList){
+    protected MetadataItem getSchemaMetadata(String schema, List<String[]> expand, DatabaseMetaData metaData) {
+        final List<MetadataItem> tableItems = expand != null ? new ArrayList<MetadataItem>() : null;
+        if (tableItems != null) {
+            final Set<String> tableNamesToExpand = new HashSet<String>();
+            for (String[] strings : expand) {
                 tableNamesToExpand.add(strings[0]);
             }
-        }
-
-        try {
-            final ResultSet tables = metaData.getTables(null, schema, null, new String[]{"TABLE", "VIEW", "ALIAS", "SYNONYM"});
-            while(tables.next()){
-                final String tableName = tables.getString("TABLE_NAME");
-                final MetadataGroupItem table = new MetadataGroupItem().setName(tableName);
-                tableItems.add(table);
-                if(tableNamesToExpand.contains(tableName)){
-                    table.setItems(expandTable(schema, tableName, metaData));
+            try {
+                final ResultSet tables = metaData.getTables(null, schema, null, new String[]{"TABLE", "VIEW", "ALIAS", "SYNONYM"});
+                while (tables.next()) {
+                    final String tableName = tables.getString("TABLE_NAME");
+                    tableItems.add(getTableMetadata(schema, tableName, tableNamesToExpand.contains(tableName), metaData));
                 }
+            } catch (SQLException e) {
+                throw new ConnectionException(e);
             }
-        } catch (SQLException e) {
-            throw new ConnectionException(e);
         }
-        return tableItems;
+        return new MetadataGroupItem().setName(schema).setItems(tableItems);
     }
 
-    protected List<MetadataItem> expandTable(String schema, String table, DatabaseMetaData metaData){
-        final ResultSet columns;
-        final List<MetadataItem> result = new ArrayList<MetadataItem>();
-        try {
-            columns = metaData.getColumns(null, schema, table, null);
-            ResultSet primaryKeySet = metaData.getPrimaryKeys(null, schema, table);
-            List<String> primaryKeys = new ArrayList<String>();
-            while (primaryKeySet.next()){
-                primaryKeys.add(primaryKeySet.getString(4));
-            }
-            final ResultSet foreignKeysSet = metaData.getImportedKeys(null, schema, table);
-            Map<String, String> foreignKeyMap = new HashMap<String, String>();
-            while (foreignKeysSet.next()){
-                String foreignKeyColumnName = foreignKeysSet.getString("FKCOLUMN_NAME");
-                String primaryKeyTableName = foreignKeysSet.getString("PKTABLE_NAME");
-                String primaryKeyColumnName = foreignKeysSet.getString("PKCOLUMN_NAME");
-                String primaryKeySchemaName = foreignKeysSet.getString("PKTABLE_SCHEM");
-                foreignKeyMap.put(foreignKeyColumnName, primaryKeySchemaName + "." + primaryKeyTableName + "." + primaryKeyColumnName);
-            }
-            while (columns.next()){
-                final String columnName = columns.getString("COLUMN_NAME");
-                int typeCode = columns.getInt("DATA_TYPE");
-                final MetadataElementItem columnItem = new MetadataElementItem().setName(columnName).setType(JDBC_TYPES_BY_CODE.get(typeCode));
-                if(primaryKeys.contains(columnName)){
-                    columnItem.setIsIdentifier(true);
+    protected MetadataItem getTableMetadata(String schema, String table, boolean expand, DatabaseMetaData metaData) {
+        MetadataGroupItem result = new MetadataGroupItem().setName(table);
+        if (expand) {
+            final List<MetadataItem> columnsMetadata = new ArrayList<MetadataItem>();
+            try {
+                final ResultSet columns = metaData.getColumns(null, schema, table, null);
+                ResultSet primaryKeySet = metaData.getPrimaryKeys(null, schema, table);
+                List<String> primaryKeys = new ArrayList<String>();
+                while (primaryKeySet.next()) {
+                    primaryKeys.add(primaryKeySet.getString(4));
                 }
-                if(foreignKeyMap.containsKey(columnName)){
-                    columnItem.setReferenceTo(foreignKeyMap.get(columnName));
+                final ResultSet foreignKeysSet = metaData.getImportedKeys(null, schema, table);
+                Map<String, String> foreignKeyMap = new HashMap<String, String>();
+                while (foreignKeysSet.next()) {
+                    String foreignKeyColumnName = foreignKeysSet.getString("FKCOLUMN_NAME");
+                    String primaryKeyTableName = foreignKeysSet.getString("PKTABLE_NAME");
+                    String primaryKeyColumnName = foreignKeysSet.getString("PKCOLUMN_NAME");
+                    String primaryKeySchemaName = foreignKeysSet.getString("PKTABLE_SCHEM");
+                    foreignKeyMap.put(foreignKeyColumnName, primaryKeySchemaName + "." + primaryKeyTableName + "." + primaryKeyColumnName);
                 }
-                result.add(columnItem);
+                while (columns.next()) {
+                    final String columnName = columns.getString("COLUMN_NAME");
+                    int typeCode = columns.getInt("DATA_TYPE");
+                    final MetadataElementItem columnItem = new MetadataElementItem().setName(columnName).setType(JDBC_TYPES_BY_CODE.get(typeCode));
+                    if (primaryKeys.contains(columnName)) {
+                        columnItem.setIsIdentifier(true);
+                    }
+                    if (foreignKeyMap.containsKey(columnName)) {
+                        columnItem.setReferenceTo(foreignKeyMap.get(columnName));
+                    }
+                    columnsMetadata.add(columnItem);
+                }
+            } catch (SQLException e) {
+                throw new ConnectionException(e);
             }
-        } catch (SQLException e) {
-            throw new ConnectionException(e);
+            result.setItems(columnsMetadata);
         }
         return result;
     }
